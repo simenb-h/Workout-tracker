109ad48e83e67abbcd022464325a43283fd7ec8e
simenb-h <simenberghansen97@gmail.com>
2020-02-14 11:12:31 +0100

    add req and proc

--- workout_tracker/__init__.py
+++ workout_tracker/__init__.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://postgresql://localhost/workouts'
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
