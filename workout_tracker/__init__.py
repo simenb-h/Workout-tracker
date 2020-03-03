497dc0a802e0d9d5c0f46116ca659912569f13aa
simenb-h <simenberghansen97@gmail.com>
2020-03-03 12:15:22 +0100

    add requriements

--- workout_tracker/__init__.py
+++ workout_tracker/__init__.py
@@ -15,8 +15,7 @@ POSTGRES = {
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sandyface@localhost/workouts'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fedthszjjfjrhg:4c8509a7bb1405f005d183d989aa589eb094050732ea36d0e2d095f1076599f1@ec2-54-197-48-79.compute-1.amazonaws.com:5432/d2lq8t5bu2tj5q'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sandyface@localhost/workouts'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
